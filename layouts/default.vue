659b1ab9bc41b0184d9571ffafbbc4e0076569ed
GalaDriel <g_ala@naver.com>
2022-12-08 11:31:19 +0900

    Add: StyleSheets

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        App
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup () {
+        
+
+        return {}
+    }
+})
+</script>
+
+<style lang="scss">
+    @import '@/assets/scss/index.scss';
+</style>
