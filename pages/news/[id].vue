082cc8806fb0ecf44b3e97afba4022955769db3f
GalaDriel <g_ala@naver.com>
2023-03-27 16:50:05 +0900

    Add: Components - List - ListTitle

--- pages/news/[id].vue
+++ pages/news/[id].vue
@@ -1,15 +1,22 @@
 <template>
     <div>
+        <ListTitle/>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-
+import ListTitle from '@/components/list/ListTitle.vue'
 export default defineComponent({
+    components:{
+        ListTitle
+    },
     setup () {
+        // https://nuxt.com/docs/guide/directory-structure/pages#router-options
+        // 참고
         definePageMeta({
-            header: true
+            header: true,
+            title:'News'
         })
 
         return {}
