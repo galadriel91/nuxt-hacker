89053227a7f31db247967e63f5879630f8b1e98c
GalaDriel <g_ala@naver.com>
2023-06-18 23:07:57 +0900

    Add: Components - Common - HackerFooter

---
+++ components/common/HackerFooter.vue
@@ -0,0 +1,83 @@
+<template>
+    <footer
+        class="footer"
+    >
+        <div class="footerWrap">
+            <div class="footerTop">
+                <div class="footerLeft">
+                    <h2>
+                        <a href="">
+                            <img
+                                src="@/assets/images/HackerNews.svg"
+                                alt="HackerNews 로고"
+                            />
+                        </a>
+                    </h2>
+                    <p>News platform for <br />modern IT</p>
+                    <span>© Copyright Hacker News</span>
+                </div>
+                <div class="footerRight">
+                    <ul>
+                        <li>
+                            <h3>Company</h3>
+                            <a href="">YC Blog</a>
+                            <a href="">Contact</a>
+                            <a href="">Press</a>
+                            <a href="">People</a>
+                            <a href="">Careers</a>
+                        </li>
+                        <li>
+                            <h3>Programs</h3>
+                            <a href="">YC Program</a>
+                            <a href="">Startup School</a>
+                            <a href="">Work at a Startup</a>
+                            <a href="">Continuity</a>
+                        </li>
+                        <li>
+                            <h3>Resources</h3>
+                            <a href="">Startup Directory</a>
+                            <a href="">Startup Library</a>
+                            <a href="">Covid</a>
+                            <a href="">Investors</a>
+                        </li>
+                        <li>
+                            <h3>ETC</h3>
+                            <a href="">Guidelines</a>
+                            <a href="">FAQ</a>
+                            <a href="">Api</a>
+                            <a href="">Security</a>
+                            <a href="">Legal</a>
+                            <a href="">Apply to YC</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footerBottom">
+                <span>© Copyright Hacker News</span>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script lang="ts" setup>
+// import { computed } from 'vue';
+// import { useRoute } from 'vue-router';
+// const route = useRoute();
+// const isJob = computed(() => {
+//     return route.name === 'jobs';
+// });
+// const isDetail = computed(() => {
+//     if (route.name === 'user' || route.name === 'post') {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// });
+// const isNot = computed(() => {
+//     return route.meta.title === 'Error';
+// });
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/components/common/HackerFooter';
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <HackerFooter/>
+</template>
+
+<script setup lang="ts">
+import HackerFooter from '../components/common/HackerFooter.vue';
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/index.scss'
+</style>
