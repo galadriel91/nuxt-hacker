97c8f843767acf916bee421dd07d807ed0ef6d85
GalaDriel <g_ala@naver.com>
2023-06-18 22:36:58 +0900

    <Nuxt Hacker Reinitial Commit>

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "default"
-declare module "C:/Users/g_ala/OneDrive/Desktop/Works/NuxtHacker/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app:{
-        head:{
-            meta: [
-                { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-            ],
-            link: [
-                { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css' }
-            ],
-        }
-    },
-    modules: [
-        [
-            '@pinia/nuxt',
-            {
-                autoImports: ['defineStore', 'acceptHMRUpdate'],
-            },
-        ]
-    ],
+  devtools: { enabled: true }
 })
